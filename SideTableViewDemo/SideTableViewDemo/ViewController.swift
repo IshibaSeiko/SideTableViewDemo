57b00b44ab24e79c06b4eac47b1d3e2eb0e292cc
IshibaSeiko <seiko.ishiba@st-ventures.jp>
2019-12-05 10:54:09 +0900

    Initial commit

---
+++ SideTableViewDemo/SideTableViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideTableViewDemo
+//
+//  Created by STV-M025 on 2019/12/05.
+//  Copyright © 2019 STV-M025. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
